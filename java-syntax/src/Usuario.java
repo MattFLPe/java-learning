565ec73e9add7c7cf6875b6265bdf5fd25aa67de
Mateus Felipe <brasil.mateus748@gmail.com>
2023-10-12 23:44:38 -0300

    Create files to practice new concepts about the language

---
+++ java-syntax/src/Usuario.java
@@ -0,0 +1,19 @@
+public class Usuario {
+
+	public static void main(String[] args) {
+
+			SmartTv smartTv = new SmartTv();
+
+
+	System.out.println("A tevê está ligada ? " + smartTv.ligada);
+	System.out.println("A tevê está em qual canal ? " + smartTv.canal);
+	System.out.println("A tevê está em qual volume ? " + smartTv.volume);
+
+	smartTv.ligar()
+	
+
+
+
+}
+
+}
